3e63f2e38387d0268d1cf868195ee0a13a4d8fc2
Jerald Liu <liu.jerald@gmail.com>
2016-06-15 13:31:43 -0400

    Added basic outline for controller. To work on monitor

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from redis import Redis
 
 app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def index():
-    return "Welcome to Superfood Minifarms!\n"
+    return "Welcome to Superfood Minifarms!"
+
+@app.route('/monitor', methods=['GET'])
+def monitor():
+    params = get_monitor_params()
+    return render_tamplate("monitor.html", params=params)
+
+@app.route('/controller', methods=['GET','POST'])
+def controller():
+    if request.method == 'POST':
+        redis.set('temp', request.form['temp'])        
+        redis.set('light', request.form['light'])        
+        redis.set('humid', request.form['humid'])        
+        redis.set('fan', request.form['fan'])
+
+    params = get_monitor_params()
+    return render_template("controller.html", params=params)
+
+def get_monitor_params():
+    params = {}
+    params['temp'] = int(redis.get('temp'))
+    params['light'] = str(redis.get('light'))
+    params['humid'] = str(redis.get('humid'))
+    params['fan'] = str(redis.get('fan'))
+    return params
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True)
